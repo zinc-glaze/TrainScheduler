d71a6912e7fca8924c39072813568f03e4015b26
Zinc Glaze <zekegraves@gmail.com>
2018-10-22 13:24:46 -0400

    coded user input and Firebase push/pull

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 //TRAIN SCHEDULER
 
-//Initialize Firebase
+//INITIALIZE FIREBASE
 
 var config = {
   apiKey: "AIzaSyDgvtVbko6celjo8pn5apM14f-WOoxH27w",
@@ -15,23 +15,23 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//Add Train click event
+//ADD TRAIN CLICK EVENT
 
-$("#add-train-button").on("click", function(event)) {
+$("#add-train-button").on("click", function(event) {
   event.preventDefault();
 
   //Gets user input
-  var trainName = $("#train-name").val.trim();
-  var destination = $("#destination").val.trim();
-  var firstTrainTime = $("#first-train-time").val.trim();
-  var frequency = $("#frequency").val.trim();
+  var trainName = $("#train-name").val().trim();
+  var destination = $("#destination").val().trim();
+  var firstTrainTime = $("#first-train-time").val().trim();
+  var frequency = $("#frequency").val().trim();
 
   //Creates temp object for train data
   var trainObject = {
-    name = trainName,
-    dest = destination,
-    ftt = firstTrainTime,
-    freq = frequency
+    name: trainName,
+    dest: destination,
+    ftt: firstTrainTime,
+    freq: frequency
   };
 
   //Pushes train data to database
@@ -50,6 +50,42 @@ $("#add-train-button").on("click", function(event)) {
   $("#destination").val("");
   $("#first-train-time").val("");
   $("#frequency").val("");
-}
+});
 
+//CREATE FIREBASE EVENT FOR ADDING NEW TRAINS TO DATABASE AND DOM
+
+//Takes snapshot of database
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  //Saves values to variables
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().dest;
+  var firstTrainTime = childSnapshot.val().ftt;
+  var frequency = childSnapshot.val().freq;
+
+  //logs new train to console
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  //Format/"prettify" first train time
+
+  //Calculate "next arrival time" and "minutes away"
+  var nextArrival;
+  var minutesAway;
+
+  //Creates new row
+  var newRow = $("#last-row").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(nextArrival),
+    $("<td>").text(minutesAway)
+  );
+
+  //Appends new row to table
+  $("#train-schedule > tbody").append(newRow);
+});
   
